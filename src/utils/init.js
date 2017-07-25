c3d0046299fa0893e2b01c5a42dc754e7537a333
borisyordanov <boris.yordanov11@yahoo.com>
2017-07-25 23:32:27 +0300

    Add: init script

---
+++ src/utils/init.js
@@ -0,0 +1,13 @@
+const init = () => {
+	let blankUserData = {
+		avatar:
+			'https://static1.squarespace.com/static/5494c048e4b0af099ee17eb8/t/56ce13f122482ef842f74b9d/1456346101630/',
+		displayName: 'user',
+		isAuthenticated: false
+	};
+
+	localStorage.setItem('userData', JSON.stringify(blankUserData));
+	localStorage.setItem('authToken', JSON.stringify({}));
+};
+
+export default init;
